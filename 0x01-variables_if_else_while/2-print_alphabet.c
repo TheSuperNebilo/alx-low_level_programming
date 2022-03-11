e17e5554869fbfe54b9be877259129b5084ec0db
root <nebilhmed6208@gmail.com>
2022-03-11 12:12:18 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+    char c;
+    
+    for (c = 'a';c<='z';c++)
+    {
+        putchar(c);
+    }
+    putchar('\n');
+	return (0);
+}
