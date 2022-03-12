bc7a217324917448ae4e362f922e13f9c6516734
root <nebilhmed6208@gmail.com>
2022-03-12 01:26:04 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,7 +5,9 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
+/* A program that prints all single digit numbers of base 10 starting from 0, followed by a new line.*/
 	int n;
+
 	for (n = 0 ; n < 10 ; n++)
 	{
 		printf("%d", n);
