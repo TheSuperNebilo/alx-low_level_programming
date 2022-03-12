22cedb9d2e1cc06e0286e3bdaad45a6475d37d2f
root <nebilhmed6208@gmail.com>
2022-03-12 01:21:48 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,16 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
 int main(void)
 {
-    int n;
-    
-    for (n = 0;n<10;n++)
-    {
-        printf("%d",n);
-        
+	int n;
+	for (n = 0;n<10;n++)
+	{
+		printf("%d",n);
 	}
+    printf("\n");
     return (0);
 }
