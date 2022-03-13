a5c323b385373583582cd420019fa59ba3c5c3f7
root <nebilhmed6208@gmail.com>
2022-03-13 13:30:05 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ *
+ */
+int main (void)
+{
+int i;
+for (i = 48 ; i <=57 ; i++)
+{
+putchar (i);
+}
+for (i = 'a' ; i <= 'f' ; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
