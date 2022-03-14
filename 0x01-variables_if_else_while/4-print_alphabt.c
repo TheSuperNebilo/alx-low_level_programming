6d9670292ad7719676e1288dd0e7de0bfffa8e88
root <nebilhmed6208@gmail.com>
2022-03-14 12:30:54 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,14 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-    char c;
-    
-    for (c = 'a';c<='z';c++)
-    {
-        if (c!='e' && c!='q')
-           {
-               putchar(c);
-           }
-    }
-   
-    putchar('\n');
-	return (0);
+char c;
+for (c = 'a';c<='z';c++)
+{
+if (c!='e' && c!='q')
+{
+putchar(c);
+}
+}   
+putchar('\n');
+return (0);
 }
