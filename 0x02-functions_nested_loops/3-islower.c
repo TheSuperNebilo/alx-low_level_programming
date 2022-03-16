51bbd0200b4510b6e0db8caf6e16b3e1e7b9158a
root <nebilhmed6208@gmail.com>
2022-03-16 01:28:34 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _islower - tests whether a character is a 
  * lowercase letter form the Enlish alphabet.
  * @c: character to test.
  * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
  */
-int_islower(int c)
+int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c > 'a' && c < 'z')
 		return (1);
 	else
 		return (0);
